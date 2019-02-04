dcaf2bf3e8fcbb96fc302f232d68339135fbbb90
John Hill <John.Hill@D2L.com>
2019-02-04 14:21:25 -0500

    Initial code commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function($) {
+  "use strict";
+
+	window.cookieconsent.initialise({
+	  "palette": {
+	    "popup": {
+	      "background": "#aa0000",
+	      "text": "#ffdddd"
+	    },
+	    "button": {
+	      "background": "#ff0000"
+	    }
+	  },
+	  "content": {
+	    "href": "www.example.com"
+	  }
+  	})
+
+})(jQuery);
